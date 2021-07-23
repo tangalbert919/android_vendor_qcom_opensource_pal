58cfe67f3aa1be04b2600733a19c81c4d3358640
yidongh <yidongh@codeaurora.org>
2021-07-23 16:25:07 +0800

    pal: check stream activity as well when set ec ref
    Rx stream may get paused and flushed by client sometimes
    and Rx graph stopped without deregistering Rx device in
    resourcemanager. This may cause Tx use this stopped device
    as ec ref and fails in Tx start.
    
    Check stream activity as well when set ec ref to avoid
    using Rx device of paused/flushed stream as ref.
    
    Change-Id: I2603ba297828038d81ba3692536688165ba854d9

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -3858,7 +3858,8 @@ std::shared_ptr<Device> ResourceManager::getActiveEchoReferenceRxDevices_l(
             }
             rx_str->getAssociatedDevices(rx_device_list);
             for (int i = 0; i < rx_device_list.size(); i++) {
-                if (!isDeviceActive_l(rx_device_list[i], rx_str))
+                if (!isDeviceActive_l(rx_device_list[i], rx_str) ||
+                    !rx_str->isActive())
                     continue;
                 deviceId = rx_device_list[i]->getSndDeviceId();
                 if (deviceId > PAL_DEVICE_OUT_MIN &&
@@ -3931,7 +3932,8 @@ std::vector<Stream*> ResourceManager::getConcurrentTxStream_l(
             }
             tx_str->getAssociatedDevices(tx_device_list);
             for (int i = 0; i < tx_device_list.size(); i++) {
-                if (!isDeviceActive_l(tx_device_list[i], tx_str))
+                if (!isDeviceActive_l(tx_device_list[i], tx_str) ||
+                    !tx_str->isActive())
                     continue;
                 deviceId = tx_device_list[i]->getSndDeviceId();
                 if (deviceId > PAL_DEVICE_IN_MIN &&
