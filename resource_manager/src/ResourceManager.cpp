3fa379df61400694b135add019f002211f20d3a6
xsang <xsang@codeaurora.org>
2021-08-13 18:25:25 +0800

    PAL: add active stream mutex for active stream search
    Active stream might be closed during search. Add active
    stream lock to avoid it.
    
    Change-Id: I898f76601a17a20238078a4ece31b5faee889183

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -7377,6 +7377,7 @@ int ResourceManager::setParameter(uint32_t param_id, void *param_payload,
         case PAL_PARAM_ID_UIEFFECT:
         {
             bool match = false;
+            mActiveStreamMutex.lock();
             std::list<Stream*>::iterator sIter;
             for(sIter = mActiveStreams.begin(); sIter != mActiveStreams.end();
                     sIter++) {
@@ -7390,6 +7391,7 @@ int ResourceManager::setParameter(uint32_t param_id, void *param_payload,
                     }
                 }
             }
+            mActiveStreamMutex.unlock();
         }
         break;
         default:
