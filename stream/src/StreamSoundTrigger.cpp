92854c464a4430d3d456f49c74a7706b33f76ab9
yidongh <quic_yidongh@quicinc.com>
2022-03-07 16:49:29 +0800

    pal: use default history buffer length if it is not set
    History buffer length wrongly set to 0 when client set
    opaque data with conf level only, causing wrong ftrt
    length provided when detection happens.
    
    Use default history buffer length if it is not set.
    
    Change-Id: I903ebb5660bd037dbdc82d0f83216a492c38adb1

--- stream/src/StreamSoundTrigger.cpp
+++ stream/src/StreamSoundTrigger.cpp
@@ -25,6 +25,38 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #define LOG_TAG "PAL: StreamSoundTrigger"
@@ -1604,10 +1636,6 @@ int32_t StreamSoundTrigger::SendRecognitionConfig(
             }
         }
     } else {
-        // get history buffer duration from sound trigger platform xml
-        hist_buf_duration_ = sm_cfg_->GetKwDuration();
-        pre_roll_duration_ = 0;
-
         if (sm_cfg_->isQCVAUUID() || sm_cfg_->isQCMDUUID()) {
             status = FillConfLevels(config, &conf_levels, &num_conf_levels);
             if (status) {
@@ -1616,6 +1644,10 @@ int32_t StreamSoundTrigger::SendRecognitionConfig(
             }
         }
     }
+
+    // use default history buffer length if it is not set
+    if (!hist_buf_duration_)
+        hist_buf_duration_ = sm_cfg_->GetKwDuration();
 
     client_capture_read_delay = sm_cfg_->GetCaptureReadDelay();
     PAL_DBG(LOG_TAG, "history buf len = %d, preroll len = %d, read delay = %d",
