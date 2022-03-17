0e6c6f7ddfc879cb0ef2f89abcc36ab302d9e2a0
yidongh <quic_yidongh@quicinc.com>
2022-03-17 11:08:32 +0800

    pal: use recognition mode from sound model
    Sometimes apk only set recognition mode in sound model
    instead of recognition config, causing SVA not working
    as wrong recognition mode set to adsp.
    
    Use recogntion mode from sound model as elite does.
    
    Change-Id: I7271f25b265ea848c898063100f904866e123fbb

--- stream/src/StreamSoundTrigger.cpp
+++ stream/src/StreamSoundTrigger.cpp
@@ -104,6 +104,7 @@ StreamSoundTrigger::StreamSoundTrigger(struct pal_stream_attributes *sattr,
     model_id_ = 0;
     sm_config_ = nullptr;
     rec_config_ = nullptr;
+    recognition_mode_ = 0;
     paused_ = false;
     device_opened_ = false;
     pending_stop_ = false;
@@ -1149,6 +1150,7 @@ int32_t StreamSoundTrigger::LoadSoundModel(
                              common_sm->data_size,
                              (uint8_t *)phrase_sm + common_sm->data_offset,
                              common_sm->data_size);
+            recognition_mode_ = phrase_sm->phrases[0].recognition_mode;
         } else {
             ar_mem_cpy(sm_config_, sizeof(*common_sm),
                              common_sm, sizeof(*common_sm));
@@ -1156,6 +1158,7 @@ int32_t StreamSoundTrigger::LoadSoundModel(
                              common_sm->data_size,
                              (uint8_t *)common_sm + common_sm->data_offset,
                              common_sm->data_size);
+            recognition_mode_ = PAL_RECOGNITION_MODE_VOICE_TRIGGER;
         }
     }
     GetUUID(&uuid, sound_model);
