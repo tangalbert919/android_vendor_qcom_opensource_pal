56599f5f48e8509255636a4370e88969ff8f8aae
Anand Mohan <quic_anamoh@quicinc.com>
2022-05-13 15:47:10 +0530

    pal: remove isAbrEnabled reset after failures in A2dp start
    Setting the isAbrEnabled to false in case of start failure will cause
    existing active stream to not call stopAbr in case of device stop.
    In case of device start failures, we do not call startAbr and we do not
    call device stop as well. Also stopAbr has checks to not process stopAbr
    if abrRefCnt is 0.
    
    Change-Id: I91037e2200abbced5dd0093bddc0510d40d6ce90
    Signed-off-by: Anand Mohan <quic_anamoh@quicinc.com>

--- device/src/Bluetooth.cpp
+++ device/src/Bluetooth.cpp
@@ -1281,14 +1281,12 @@ int BtA2dp::start()
 
     status = (a2dpRole == SOURCE) ? startPlayback() : startCapture();
     if (status) {
-        isAbrEnabled = false;
         goto exit;
     }
 
     if (totalActiveSessionRequests == 1) {
         status = configureSlimbusClockSrc();
         if (status) {
-            isAbrEnabled = false;
             goto exit;
         }
     }
